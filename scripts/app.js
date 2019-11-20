b4a032d1b0fe4871631d014e175a7491d1ef6fb7
Yash Parekh <parekhyashj@gmail.com>
2019-11-19 19:08:02 -0500

    completed work of day/night picture and icons

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,9 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img')
+console.log(icon,time);
 
 const updateUI = (data)=>{
   // const cityDetails = data.cityDetails;
@@ -8,6 +11,7 @@ const updateUI = (data)=>{
 
   // destructuring properties
   const {cityDetails, weather} = data;
+  console.log(weather.WeatherIcon);
 
   // updating the details template
   details.innerHTML = `
@@ -17,6 +21,19 @@ const updateUI = (data)=>{
     <span>${weather.Temperature.Metric.Value}</span>
     <span>&deg;</span>
   </div>`;
+
+  // get the time of day and chage image accordingly
+  let isDayTime = weather.IsDayTime;
+
+  // setting the icon according to the WeatherIcon response in the weather end point
+  icon.setAttribute('src',`./img/icons/${weather.WeatherIcon}.svg`);
+
+  if(isDayTime){
+    // as source is an attribur we will use setAttribute method on that image
+    time.setAttribute('src','./img/day.svg');
+  }else{
+    time.setAttribute('src','./img/night.svg');
+  }
 
   // remove the d-none class if present
   if(card.classList.contains('d-none')){
